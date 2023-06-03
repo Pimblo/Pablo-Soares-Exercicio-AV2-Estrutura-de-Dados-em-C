56cc4fab83cba30350f27a94b761e9789cb7961d
Pablo <109488818+Pimblo@users.noreply.github.com>
2023-06-03 15:19:18 -0300

    Update sort.h

--- Exercício Avaliattivo AV2/Questão 1/sort.h
+++ Exercício Avaliattivo AV2/Questão 1/sort.h
@@ -9,11 +9,13 @@ void bubbleSort(int vet[], int n) {
         }
     }
 }
+
 void insertSort(vet[], int n) {
   int i, j, k;
   for(i = 0; i <n; i++){
     k = vet[i];
     j = i - 1;
+
     while(j >= 0 && vet[j] > k){
       vet[j +1] = vet[j];
       j = j - 1;
@@ -21,6 +23,7 @@ void insertSort(vet[], int n) {
     vet[j + 1] = k;
   }
 }
+
 void printVet(int vet[], int n){
   for(int i = 0; i < n; i++){
     printf("%d ", vet[i]);
